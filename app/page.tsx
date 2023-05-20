e180c23ef35917483db6ab4b3469fd7a5733d11f
Mario Alemán <mar.alemanf@gmail.com>
2023-05-20 15:17:49 -0600

    Add: word to h1 in / directory

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Layout from '@src/components/Layout'
 export default function Home () {
   return (
     <Layout>
-      <h1>Hola este es la página web para infojobs</h1>
+      <h1>Hola este es la página web para infojobs - hackathon</h1>
     </Layout>
   )
 }
